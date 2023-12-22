822fec4dc2e9b7bbc02a01831528706dfb3a1186
jankoczuba <jan.koczuba@it-solve.pl>
2023-12-22 21:57:31 +0100

    Initial Commit

---
+++ swift-ecommerce-furniture/swift_ecommerce_furnitureApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ecommerce_furnitureApp.swift
+//  swift-ecommerce-furniture
+//
+//  Created by Jan Koczuba on 22/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct swift_ecommerce_furnitureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
